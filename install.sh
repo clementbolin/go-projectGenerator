a76270c3ba3537421a992d6715822d4d6d7824fc
Bolin <clement.bolin@epitech.eu>
2020-09-08 15:44:25 +0200

    update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
+if [[ $EUID -ne 0 ]]; then
+    echo "You must run this with superuser priviliges.  Try \"sudo ./install\"" 2>&1
+    exit 1
+else
+    echo "Installing go-ProjectGenerator..."
 make build
 sudo cp ./bin/go-projectGenerator /usr/local/bin
-echo -ne "go-ProjectGenerator install"
+echo "go-ProjectGenerator install"
